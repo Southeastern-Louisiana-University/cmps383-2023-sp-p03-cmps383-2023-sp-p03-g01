e94e9b3c9d055c1224a2e4cd832f3b5fa4ce0960
scethen <Kyle.Tran@selu.edu>
2023-02-28 10:38:55 -0600

    TrainRoute

---
+++ SP23.P03.Web/Features/TrainRoutes/TrainRoute.cs
@@ -0,0 +1,16 @@
+﻿using SP23.P03.Web.Features.TrainStations;
+
+namespace SP23.P03.Web.Features.TrainRoutes
+{
+    public class TrainRoute
+    {
+        public int Id { get; set; }
+        public DateTimeOffset ArrivalTime { get; set; }
+        public DateTimeOffset DeperatureTime { get; set; }
+        public int StartingTrainStationId { get; set; }
+        public TrainStation StartingTrainStation { get; set; } = new TrainStation();
+
+        public int EndingTrainStationId { get; set; }
+        public TrainStation EndingTrainStation { get; set; } = new TrainStation();
+    }
+}
