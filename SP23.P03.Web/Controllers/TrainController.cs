015544a4582cce3ad70ef64f76117dd43ddb7f34
scethen <Kyle.Tran@selu.edu>
2023-02-28 23:38:37 -0600

    TrainController

--- SP23.P03.Web/Controllers/TrainController.cs
+++ SP23.P03.Web/Controllers/TrainController.cs
@@ -5,6 +5,7 @@ using SP23.P03.Web.Data;
 using SP23.P03.Web.Extensions;
 using SP23.P03.Web.Features.Authorization;
 using SP23.P03.Web.Features.Trains;
+using SP23.P03.Web.Features.TrainStations;
 
 namespace SP23.P03.Web.Controllers;
 
@@ -25,17 +26,118 @@ namespace SP23.P03.Web.Controllers;
     {
         return GetTrainDtos(trains);
     }
+
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<TrainDto> GetStationById(int id)
+    {
+        var result = GetTrainDtos(trains.Where(x => x.Id == id)).FirstOrDefault();
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public ActionResult<TrainCreateDto> CreateStation(TrainCreateDto dto)
+    {
+        if (IsInvalid(dto))
+        {
+            return BadRequest();
+        }
+
+        var train = new Train
+        {
+            Model = dto.Model,
+            Capacity = dto.Capacity,
+            TrainRouteId = dto.TrainRouteId,
+        };
+        trains.Add(train);
+
+        dataContext.SaveChanges();
+
+        dto.Id = train.Id;
+
+        return CreatedAtAction(nameof(GetStationById), new { id = dto.Id }, dto);
+    }
+
+    private bool IsInvalid(TrainCreateDto dto)
+    {
+        return string.IsNullOrWhiteSpace(dto.Model) ||
+               dto.Model.Length > 100 ||
+               string.IsNullOrWhiteSpace((dto.Capacity).ToString());
+    }
     private static IQueryable<TrainDto> GetTrainDtos(IQueryable<Train> trains)
     {
         return trains
             .Select(x => new TrainDto
             {
                 Id = x.Id,
-                Capacity= x.Capacity,
-                Features= x.Features,
-                Model= x.Model,
-                Sections= x.Sections,
+                Capacity = x.Capacity,
+                Features = x.Features,
+                Model = x.Model,
+                Sections = x.Sections,
                 TrainRouteId = x.TrainRouteId,
             });
     }
+    [HttpPut]
+    [Route("{id}")]
+    public ActionResult<TrainCreateDto> UpdateTrain(int id, TrainCreateDto dto)
+    {
+        if (IsInvalid(dto))
+        {
+            return BadRequest();
+        }
+
+        var train = trains.FirstOrDefault(x => x.Id == id);
+        if (train == null)
+        {
+            return NotFound();
+        }
+
+
+        train.Model = dto.Model;
+        train.Capacity = dto.Capacity;
+        train.TrainRouteId = dto.TrainRouteId;
+
+        dataContext.SaveChanges();
+
+        dto.Id = train.Id;
+
+        return Ok(dto);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public ActionResult DeleteTrain(int id)
+    {
+        var train = trains.FirstOrDefault(x => x.Id == id);
+        if (train == null)
+        {
+            return NotFound();
+        }
+
+
+        trains.Remove(train);
+
+        dataContext.SaveChanges();
+
+        return Ok();
+    }
+    /*    private bool InvalidStationId(int? StationId)
+        {
+            if (StationId == null)
+            {
+                return false;
+            }
+
+            if (!User.IsInRole(RoleNames.Admin))
+            {
+                // only admins can change manager ids anyway
+                return false;
+            }
+            return !dataContext.Set<TrainStation>().Any(x => x.Id == StationId);
+        }*/
 }
---
+++ SP23.P03.Web/Features/Trains/TrainCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SP23.P03.Web.Features.Trains
+{
+    public class TrainCreateDto
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public int? TrainRouteId { get; set; }
+    }
+}
