a0f30be93224959c7ad0eb54282a913d041e2f00
scethen <Kyle.Tran@selu.edu>
2023-02-28 21:04:02 -0600

    traincontroller

---
+++ SP23.P03.Web/Controllers/TrainController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Extensions;
+using SP23.P03.Web.Features.Authorization;
+using SP23.P03.Web.Features.Trains;
+
+namespace SP23.P03.Web.Controllers;
+
+[Route("api/trains")]
+[ApiController]
+    public class TrainController : ControllerBase
+    {
+        private readonly DbSet<Train> trains;
+        private readonly DataContext dataContext;
+
+        public TrainController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+            trains = dataContext.Set<Train>();
+        }
+    [HttpGet]
+    public IQueryable<TrainDto> GetAllTrains()
+    {
+        return GetTrainDtos(trains);
+    }
+    private static IQueryable<TrainDto> GetTrainDtos(IQueryable<Train> trains)
+    {
+        return trains
+            .Select(x => new TrainDto
+            {
+                Id = x.Id,
+                Capacity= x.Capacity,
+                Features= x.Features,
+                Model= x.Model,
+                Sections= x.Sections,
+                TrainRouteId = x.TrainRouteId,
+            });
+    }
+}
---
+++ SP23.P03.Web/Features/Trains/TrainDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SP23.P03.Web.Features.Trains
+{
+    public class TrainDto
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+        public int? TrainRouteId { get; set; }
+        public List<Section>? Sections { get; set; }
+        public List<Feature>? Features { get; set; }
+    }
+}
