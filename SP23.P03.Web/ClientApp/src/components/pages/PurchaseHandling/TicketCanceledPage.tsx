6d7c42bfaba97ede7d8a06482cdecbde2e4707a9
Stacie L <Nicklaja00@gmail.com>
2023-03-15 15:35:18 -0500

    added landing pages for stripe

---
+++ SP23.P03.Web/ClientApp/src/components/pages/PurchaseHandling/TicketCanceledPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import { useViewportSize } from '@mantine/hooks';
+import { getMantineComponentSize } from '../../../util/getMantineComponentSize';
+import { AppRoutes } from '../../../models/AppRoutes';
+import { TICKET_CANCELED_PAGE_STYLING } from './TicketCanceledPageStyling';
+
+interface TicketCanceledPage {}
+/**
+ * <description here>
+ *
+ * @param props <description here>
+ */
+
+export function TicketCanceledPage(props: TicketCanceledPage): React.ReactElement {
+
+  const navigate = useNavigate();
+  const { width: browserWidth } = useViewportSize();
+  const componentSize = getMantineComponentSize(browserWidth);
+
+  const navigateToHomePage = () => {
+    navigate(AppRoutes.HOME);
+  };
+
+  return (
+    <div style={TICKET_CANCELED_PAGE_STYLING.rootStyles}>
+      {/* This is a placeholder that launches the Stripe payment */}
+      <span>Purchase canceled.</span>
+      <Button size={componentSize} onClick={navigateToHomePage}>
+        Home
+      </Button>
+    </div>
+  );
+}
