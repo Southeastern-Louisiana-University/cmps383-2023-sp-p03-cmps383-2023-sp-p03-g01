a2633f4d467c06a2fefd8043e1e8879f35620e87
Kyle Tran <Kyle.Tran@selu.edu>
2023-04-10 21:45:58 -0500

    Add the Dto

--- SP23.P03.Web/Features/ScheduledRoutes/TrainScheduledRoutesDto.cs
+++ SP23.P03.Web/Features/ScheduledRoutes/TrainScheduledRoutesDto.cs
@@ -1,6 +1,10 @@
-﻿namespace SP23.P03.Web.Features.ScheduledRoutes
+﻿using SP23.P03.Web.Features.Route;
+
+namespace SP23.P03.Web.Features.ScheduledRoutes
 {
     public class TrainScheduledRoutesDto
     {
+        public int Id { get; set; }
+        public IEnumerable<TrainRouteDto>? Routes { get; set; }
     }
 }
