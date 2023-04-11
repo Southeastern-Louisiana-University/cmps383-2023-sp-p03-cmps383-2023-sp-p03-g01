b2bf3999a97b8f257808686c70fa8565c5034b76
Kyle Tran <Kyle.Tran@selu.edu>
2023-04-10 21:28:35 -0500

    Get api

---
+++ SP23.P03.Web/Controllers/ScheduledRoutesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SP23.P03.Web.Data;
+using SP23.P03.Web.Features.Route;
+using SP23.P03.Web.Features.ScheduledRoutes;
+using SP23.P03.Web.Features.Trains;
+
+namespace SP23.P03.Web.Controllers;
+[Route("api/scheduledRoutes")]
+[ApiController]
+    public class ScheduledRoutesController : ControllerBase
+    {
+    private readonly DbSet<TrainScheduledRoutes> scheduledRoutes;
+    private readonly DataContext dataContext;
+
+    public ScheduledRoutesController(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+        scheduledRoutes = dataContext.Set<TrainScheduledRoutes>();
+    }
+    private static IQueryable<TrainScheduledRoutesDto> GetTrainScheduledRoutesDtos(IQueryable<TrainScheduledRoutes> scheduledRoutes)
+    {
+        return scheduledRoutes
+            .Select(x => new TrainScheduledRoutesDto
+            {
+                Id = x.Id,
+                Routes = x.Routes.Select(x => new TrainRouteDto
+                {
+                    Id = x.Id,
+                    ArrivalTime = x.ArrivalTime,
+                    DeperatureTime = x.DeperatureTime,
+                    PathId = x.PathId,
+                    TrainId = x.Train.Id,
+                }),
+            });
+    }
+    [HttpGet]
+    public IQueryable<TrainScheduledRoutesDto> GetAllTrainScheduledRoutes()
+    {
+        return GetTrainScheduledRoutesDtos(scheduledRoutes);
+    }
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<TrainScheduledRoutesDto> GetSectionById(int id)
+    {
+        var result = GetTrainScheduledRoutesDtos(scheduledRoutes.Where(x => x.Id == id)).FirstOrDefault();
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+}
---
+++ SP23.P03.Web/Features/ScheduledRoutes/TrainScheduledRoutesDto.cs
@@ -0,0 +1,6 @@
+﻿namespace SP23.P03.Web.Features.ScheduledRoutes
+{
+    public class TrainScheduledRoutesDto
+    {
+    }
+}
