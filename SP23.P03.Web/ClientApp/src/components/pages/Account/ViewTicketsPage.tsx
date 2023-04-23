543f681476b5fce0942b5d188316861393bacf24
Reece Bourgeois <25267927+reecewbourgeois@users.noreply.github.com>
2023-04-23 18:04:30 -0500

    get user tickets getting there

--- SP23.P03.Web/ClientApp/src/components/pages/Account/ViewTicketsPage.tsx
+++ SP23.P03.Web/ClientApp/src/components/pages/Account/ViewTicketsPage.tsx
@@ -1,10 +1,13 @@
 import { Button, Flex, Stack, Title } from '@mantine/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SeatType } from '../../../models/SeatTypes';
 import { TicketSummaryProps, TicketSummary } from '../../common/TicketSummary';
 import { TicketModal } from '../../common/TicketModal';
 import { useViewportSize } from '@mantine/hooks';
 import { getMantineComponentSize } from '../../../util/getMantineComponentSize';
+import API from '../../../util/entrackApi';
+import { TrainRouteTicketDto } from '../../../api/EntrackApi.ts/EntrackApi';
+import dayjs from 'dayjs';
 
 interface TicketContainerProps {
     ticket: TicketSummaryProps;
@@ -42,17 +45,38 @@ const TicketContainer = ({ ticket }: TicketContainerProps): React.ReactElement =
  * Displays the tickets that the user has purchased.
  */
 export function ViewTicketsPage(): React.ReactElement {
-    const TEST_DATA: TicketSummaryProps = {
-        departureStation: 'Hammond, LA',
-        arrivalStation: 'Baton Rouge, LA',
-        departureTime: '12:01pm',
-        arrivalTime: '2:15pm',
-        duration: '2hr 14min',
-        layover: null,
-        seat: SeatType.COACH,
-        cost: 152,
-        passengerCount: 1,
-    };
+    const [ticketsToDisplay, setTicketsToDisplay] = useState<TicketSummaryProps[]>([]);
+
+    useEffect(() => {
+        API.api.authenticationMeList().then((response) => {
+            if (response.data.tickets === undefined || response.data.tickets === null) return;
+
+            // First, filter out only the tickets that are coach tickets
+            const coachOnlyTickets = response.data.tickets.filter((ticket) => ticket.seatType === SeatType.COACH);
+
+            // Then, group the tickets by the date of the trip
+            const ticketsByDate: { [key: string]: TrainRouteTicketDto[] } = coachOnlyTickets.reduce((acc, ticket) => {
+                const date = dayjs(ticket.trainRoute!.departureTime).format('MMM D, YYYY');
+
+                if (acc[date] === undefined) {
+                    acc[date] = [ticket];
+                } else {
+                    acc[date]!.push(ticket);
+                }
+
+                return acc;
+            }, {} as { [key: string]: TrainRouteTicketDto[] });
+
+            // Then, sort the tickets for each date by the departure time
+            Object.keys(ticketsByDate).forEach((date) => {
+                ticketsByDate[date]!.sort((a, b) => {
+                    return dayjs(a.trainRoute!.departureTime).unix() - dayjs(b.trainRoute!.departureTime).unix();
+                });
+            });
+
+            console.log(ticketsByDate);
+        });
+    }, []);
 
     return (
         <div
@@ -71,7 +95,7 @@ export function ViewTicketsPage(): React.ReactElement {
                     wrap='wrap'
                     gap='2rem'
                 >
-                    <TicketContainer ticket={TEST_DATA} />
+                    {/* <TicketContainer ticket={} /> */}
                 </Flex>
             </Stack>
 
@@ -81,7 +105,7 @@ export function ViewTicketsPage(): React.ReactElement {
                     wrap='wrap'
                     gap='2rem'
                 >
-                    <TicketContainer ticket={TEST_DATA} />
+                    {/* <TicketContainer ticket={TEST_DATA} /> */}
                 </Flex>
             </Stack>
         </div>
